7bb24f44d85a7247c206c0aced4c369ff12cedba
Александр Фролов <froller@froller.net>
2020-11-05 03:29:01 +0300

    Исправлены макросы
    Укоротил запись добавления вершин и элементов

--- Graphene/simpleobjects.cpp
+++ Graphene/simpleobjects.cpp
@@ -2,38 +2,38 @@
 #include <math.h>
 #include <array>
 
-#define VERTEX(x,y,z,r,g,b) {position: fvec3(x, y, z), color: fvec3(r, g, b)}
-#define ELEMENT(a,b,c) {a, b, c}
+#define VERTEX(x,y,z,r,g,b) m_Vertices.push_back({position: fvec3(x, y, z), color: fvec3(r, g, b)})
+#define ELEMENT(a,b,c) m_Primitives.push_back({a, b, c})
 
 Graphene::SimpleObjects::Triangle::Triangle()
 {
-    m_Vertices.push_back(VERTEX( 0.f,  1.f / sqrtf(3.f),  0.f,  0.f, 1.f, 0.f));
-    m_Vertices.push_back(VERTEX(-0.5, -0.5 / sqrtf(3.f),  0.f,  0.f, 1.f, 0.f));
-    m_Vertices.push_back(VERTEX( 0.5, -0.5 / sqrtf(3.f),  0.f,  0.f, 1.f, 0.f));
-    m_Primitives.push_back(ELEMENT(0, 1, 2));
+    VERTEX( 0.f,  1.f / sqrtf(3.f),  0.f,  0.f, 1.f, 0.f);
+    VERTEX(-0.5, -0.5 / sqrtf(3.f),  0.f,  0.f, 1.f, 0.f);
+    VERTEX( 0.5, -0.5 / sqrtf(3.f),  0.f,  0.f, 1.f, 0.f);
+    ELEMENT(0, 1, 2);
 }
 
 Graphene::SimpleObjects::Square::Square()
 {
-    m_Vertices.push_back(VERTEX(-0.5, -0.5, 0.f, 1.f, 0.f, 0.f));
-    m_Vertices.push_back(VERTEX(-0.5,  0.5, 0.f, 1.f, 0.f, 0.f));
-    m_Vertices.push_back(VERTEX( 0.5,  0.5, 0.f, 1.f, 0.f, 0.f));
-    m_Vertices.push_back(VERTEX( 0.5, -0.5, 0.f, 1.f, 0.f, 0.f));
-    m_Primitives.push_back(ELEMENT(0, 2, 1));
-    m_Primitives.push_back(ELEMENT(0, 2, 3));
+    VERTEX(-0.5, -0.5, 0.f, 1.f, 0.f, 0.f);
+    VERTEX(-0.5,  0.5, 0.f, 1.f, 0.f, 0.f);
+    VERTEX( 0.5,  0.5, 0.f, 1.f, 0.f, 0.f);
+    VERTEX( 0.5, -0.5, 0.f, 1.f, 0.f, 0.f);
+    ELEMENT(0, 2, 1);
+    ELEMENT(0, 2, 3);
 }
 
 Graphene::SimpleObjects::Tetrahedron::Tetrahedron()
 {
-    m_Vertices.push_back(VERTEX( 0.f,  sqrtf(6.f) / -12.f,  1.f / sqrtf(3.f),  0.f, 1.f, 1.f));
-    m_Vertices.push_back(VERTEX(-0.5,  sqrtf(6.f) / -12.f, -0.5 / sqrtf(3.f),  0.f, 1.f, 1.f));
-    m_Vertices.push_back(VERTEX( 0.5,  sqrtf(6.f) / -12.f, -0.5 / sqrtf(3.f),  0.f, 1.f, 1.f));
-    m_Vertices.push_back(VERTEX( 0.f,  0.5 * sqrtf(1.f),   -0.f,               0.f, 1.f, 1.f));
+    VERTEX( 0.f,  sqrtf(6.f) / -12.f,  1.f / sqrtf(3.f),  0.f, 1.f, 1.f);
+    VERTEX(-0.5,  sqrtf(6.f) / -12.f, -0.5 / sqrtf(3.f),  0.f, 1.f, 1.f);
+    VERTEX( 0.5,  sqrtf(6.f) / -12.f, -0.5 / sqrtf(3.f),  0.f, 1.f, 1.f);
+    VERTEX( 0.f,  0.5 * sqrtf(1.f),   -0.f,               0.f, 1.f, 1.f);
 
-    m_Primitives.push_back(ELEMENT(0, 1, 2));
-    m_Primitives.push_back(ELEMENT(0, 3, 1));
-    m_Primitives.push_back(ELEMENT(2, 3, 0));
-    m_Primitives.push_back(ELEMENT(1, 3, 2));
+    ELEMENT(0, 1, 2);
+    ELEMENT(0, 3, 1);
+    ELEMENT(2, 3, 0);
+    ELEMENT(1, 3, 2);
 }
 
 #undef ELEMENT
