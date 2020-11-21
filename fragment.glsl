f35dbcfa256c99e4d509a7f0fc4eb5ad79aecf0c
Александр Фролов <froller@froller.net>
2020-11-21 04:23:35 +0300

    Структура для передачи данных во фргментный шейдер

--- fragment.glsl
+++ fragment.glsl
@@ -1,23 +1,27 @@
 #version 460 core
 
-in vec3 position;
-in vec3 normal;
-in vec3 color;
+struct Vertex {
+    vec3 position;
+    vec3 normal;
+    vec3 color;
+};
 
-uniform mat4 MVP[3];
-uniform vec3 cameraPosition;
-uniform vec3 ambientColor;
-uniform uint lightsCount;
-
-struct lightSource {
+struct LightSource {
     vec3 position;
     float reserved0;
     vec3 color;
     float attenuation;
 };
 
+in Vertex vertex;
+
+uniform mat4 MVP[3];
+uniform vec3 cameraPosition;
+uniform vec3 ambientColor;
+uniform uint lightsCount;
+
 layout (std430, binding = 0) buffer Lights {
-    lightSource light[];
+    LightSource light[];
 } lights;
 
 out vec3 fragmentColor;
@@ -28,16 +32,16 @@ void main()
     vec3 specular = vec3(0);
     for (uint i = 0; i < lightsCount; ++i)
     {
-        vec3 lightingDirection = normalize(lights.light[i].position - position);
-        float lightingDistance = length(lights.light[i].position - position);
-        vec3 viewDirection = normalize(cameraPosition - position);
+        vec3 lightingDirection = normalize(lights.light[i].position - vertex.position);
+        float lightingDistance = length(lights.light[i].position - vertex.position);
+        vec3 viewDirection = normalize(cameraPosition - vertex.position);
         vec3 bisect = normalize(lightingDirection + viewDirection);
 
         //vec3 dimmedColor = lights.light[i].color;
         vec3 dimmedColor = lights.light[i].color * pow(clamp((lights.light[i].attenuation - length(lightingDistance)) / lights.light[i].attenuation, 0.f, 1.f), 2);
 
-        diffuse = diffuse + max(dot(normalize(normal), lightingDirection), 0.0) * dimmedColor;
-        specular = specular + pow(max(dot(normalize(normal), bisect), 0.0), 64) * dimmedColor;
+        diffuse = diffuse + max(dot(normalize(vertex.normal), lightingDirection), 0.0) * dimmedColor;
+        specular = specular + pow(max(dot(normalize(vertex.normal), bisect), 0.0), 64) * dimmedColor;
     }
     fragmentColor = specular + diffuse + ambientColor;
 }
--- vertex.glsl
+++ vertex.glsl
@@ -1,20 +1,23 @@
 #version 460 core
 
+struct Vertex {
+    vec3 position;
+    vec3 normal;
+    vec3 color;
+};
+
 layout(location = 0) in vec3 vertexPosition;
 layout(location = 1) in vec3 vertexNormal;
 layout(location = 2) in vec3 vertexColor;
 
-//out vec2 UV;
-out vec3 position;
-out vec3 normal;
-out vec3 color;
+out Vertex vertex;
 
 uniform mat4 MVP[3];
 
 void main()
 {
     gl_Position = MVP[2] * MVP[1] * MVP[0] * vec4(vertexPosition, 1);
-    position = vertexPosition;
-    normal = vertexNormal;
-    color = vertexColor;
+    vertex.position = vertexPosition;
+    vertex.normal = vertexNormal;
+    vertex.color = vertexColor;
 }
