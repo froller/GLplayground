e9252c9b971c1be4078a66559d927df8c8e54085
Александр Фролов <froller@froller.net>
2020-11-23 11:11:57 +0300

    Поднял методы изменения состояния объекта
    position, rotation и scale

--- Graphene/object.cpp
+++ Graphene/object.cpp
@@ -11,3 +11,33 @@ fmat4 Graphene::Object::transform() const
     tm = glm::translate(glm::fmat4(1), m_Position) * tm;
     return tm;
 }
+
+fvec3 Graphene::Object::position() const
+{
+    return m_Position;
+}
+
+void Graphene::Object::position(const fvec3 position)
+{
+    m_Position = position;
+}
+
+fquat Graphene::Object::rotation() const
+{
+    return m_Rotation;
+}
+
+void Graphene::Object::rotation(const fquat rotation)
+{
+    m_Rotation = rotation;
+}
+
+fvec3 Graphene::Object::scale() const
+{
+    return m_Scale;
+}
+
+void Graphene::Object::scale(const fvec3 scale)
+{
+    m_Scale = scale;
+}
--- Graphene/object.h
+++ Graphene/object.h
@@ -18,6 +18,12 @@ public:
     Object(const fvec3 position = { 0, 0, 0 }, const fquat rotation = { 0, 0, 0, 1 }, const fvec3 scale = { 1, 1, 1 })
         : m_Position(position), m_Rotation(rotation), m_Scale(scale) {};
     virtual ~Object() = default;
+    virtual fvec3 position() const;
+    virtual void position(const fvec3 position);
+    virtual fquat rotation() const;
+    virtual void rotation(const fquat rotation);
+    virtual fvec3 scale() const;
+    virtual void scale(const fvec3 scale);
     virtual fmat4 transform() const;
 };
 
